cd0a36c289bf5f218fd317dc9b9302a8e663b1ca
Kei-Eff <karl.alberto@hotmail.com>
2021-07-14 00:41:10 +1000

    Finished onboarding game data to json file

--- jsontest.py
+++ jsontest.py
@@ -1,14 +1,13 @@
 import json
 
-# y = json.loads("test.json")
+with open("gamesList.json", "r") as json_file:
+    data = json.load(json_file)
 
-with open("test.json", "r") as json_file:
-    y = json.load(json_file)
-    filtered = filter(lambda game: 2 in game["players"], y["boardgames"])
-    print(list(filtered))
+filtered = filter(lambda game: 2 in game["players"], data["boardgames"])
+print(list(filtered))
 
-with open("test.json", "r") as file:
-    filterGenre = filter(lambda game: game["genre"] == "Mystery", y["boardgames"])
-    print(list(filterGenre))
+# with open("test.json", "r") as file:
+#     filterGenre = filter(lambda game: game["genre"] == "Mystery", y["boardgames"])
+#     print(list(filterGenre))
 
 # print(y["boardgames"])
